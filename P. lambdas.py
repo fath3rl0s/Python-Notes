0d45afd3b6247f79b995a132ca706083799a0e6f
Carlos Enamorado <88733190+fath3rl0s@users.noreply.github.com>
2023-06-07 11:42:42 -0400

    Update P. lambdas.py

--- P. lambdas.py
+++ P. lambdas.py
@@ -78,6 +78,29 @@ print(blocks("string",2))
 print(blocks("strings", 3))
 
 
+'''
+Imagine you have a long string of numbers: "1234567890".
+
+Now, let's say you want to divide this string into smaller blocks of size 3. The lambda function you provided, blocks = lambda x, y: [x[i:i + y] for i in range(0, len(x), y)], can help you achieve that.
+
+Here's a step-by-step explanation of how it works:
+
+The lambda function takes two inputs: x (the long string of numbers) and y (the desired block size, which is 3 in our example).
+
+The range(0, len(x), y) part generates a sequence of numbers starting from 0 and incrementing by y each time. In our example, it generates the sequence: 0, 3, 6, 9. These numbers represent the starting indices of the blocks in the original string.
+
+The lambda function then iterates over the generated indices using a list comprehension. For each index i, it slices the original string x to extract a block of length y. In our example, it slices the string as follows:
+
+For i = 0, it takes the slice x[0:3] and gets the block "123".
+For i = 3, it takes the slice x[3:6] and gets the block "456".
+For i = 6, it takes the slice x[6:9] and gets the block "789".
+For i = 9, it takes the slice x[9:12], but since the string ends before index 12, it just takes the remaining part "0".
+The resulting blocks "123", "456", "789", and "0" are collected in a list, which is then returned by the lambda function.
+
+So, when you apply this lambda function to the string "1234567890" with a block size of 3, it will return a list of blocks: ["123", "456", "789", "0"].
+'''
+
+
 print("\n ")
 
 
